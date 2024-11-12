da27076634600d91f1f7b14099f19893443d1f5a
pavvashy <julinnaea@gmail.com>
2024-11-11 21:17:55 -0800

    add files via upload
    index.html and javascript.js files added. the javascript.js file just has a bunch of variable and console.log tests

---
+++ javascript.js
@@ -0,0 +1,26 @@
+let a = 10
+console.log (9*a)
+let b = 7*a
+console.log (b)
+
+console.log (23+97)
+console.log (6,6,6,6,6,6)
+console.log ((4 + 6 + 9) / 77)
+
+const max = 57
+const actual = max+13
+const percentage = actual % max
+let min = percentage+10
+let numbers = "57" + "87"
+console.log (min)
+console.log (numbers)
+
+const decimal = 3.141592653589793238462643383279502884197
+const simplifiedDecimal = decimal.toFixed(2);
+console.log (simplifiedDecimal)
+
+let am = 2
+let bm = am
+let cm = am++
+console.log (am)
+console.log (cm)
